bdca31006710523cc70dcdb9b1319096147de7ff
CodeUpJewell <94193721+CodeUpJewell@users.noreply.github.com>
2021-12-09 17:25:34 -0600

    Add files via upload
    My outputs.tf file

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+# TODO: Define the output variable for the lambda function.
+output "lambda_arn"{ 
+    description = "ARN of Udacity Lambda Function" 
+    value = "arn:aws:lambda:us-east-1:510617822936:function:boboaudacity"
+}

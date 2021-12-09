55c75253876a7bd0fe80ba0b18bc8a046599a525
CodeUpJewell <94193721+CodeUpJewell@users.noreply.github.com>
2021-12-09 17:50:13 -0600

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,5 +0,0 @@
-# TODO: Define the output variable for the lambda function.
-output "lambda_arn"{ 
-    description = "ARN of Udacity Lambda Function" 
-    value = "arn:aws:lambda:us-east-1:510617822936:function:boboaudacity"
-}

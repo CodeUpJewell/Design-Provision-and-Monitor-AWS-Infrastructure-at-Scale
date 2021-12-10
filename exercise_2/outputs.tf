2e750f0589a5b1ddd0527bc9731131b51a6e6b4a
CodeUpJewell <94193721+CodeUpJewell@users.noreply.github.com>
2021-12-09 22:21:26 -0600

    Add files via upload

---
+++ exercise_2/outputs.tf
@@ -0,0 +1,5 @@
+# TODO: Define the output variable for the lambda function.
+output "lambda_arn"{
+    description = "ARN of Udacity Lambda Function"
+    value = "{arn:aws:iam::510617822936:role/IamLambduh}"
+}
